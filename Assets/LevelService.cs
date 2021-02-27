e20052244591ee02568ad6ff1e37044905c4cabe
Ryan Zander <exide@hotmail.com>
2021-02-27 09:58:21 -0800

    Pick a random background color from a list.

--- Assets/LevelService.cs
+++ Assets/LevelService.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelService : MonoBehaviour {
@@ -14,7 +15,15 @@ public class LevelService : MonoBehaviour {
     [SerializeField]
     private int currentLevel = 1;
 
+    private List<Color> availableBackgroundColors = new List<Color> {
+        Color.blue, Color.cyan, Color.green, Color.magenta, Color.red, Color.yellow
+    };
+
+    private Camera mainCamera;
+
     public void Start() {
+        mainCamera = Camera.main;
+        mainCamera.backgroundColor = pickRandomColor();
         spawnAnimalsForLevel();
     }
 
@@ -22,10 +31,22 @@ public class LevelService : MonoBehaviour {
         int animalsSpawned = GameObject.FindGameObjectsWithTag("Animal").Length;
         if (animalsSpawned > 0) return;
 
-        currentLevel++;
+        goToNextLevel();
         spawnAnimalsForLevel();
     }
 
+    private void goToNextLevel() {
+        mainCamera.backgroundColor = pickRandomColor();
+        currentLevel++;
+    }
+
+    private Color pickRandomColor() {
+        int count = availableBackgroundColors.Count;
+        int pick = Random.Range(0, count - 1);
+        Color baseColor = availableBackgroundColors[pick];
+        return baseColor * 0.5f;
+    }
+    
     private void spawnAnimalsForLevel() {
         int animalCount = currentLevel * animalsPerLevelMultiplier;
         animalSpawner.spawnAnimals(animalToSpawn, animalCount);
